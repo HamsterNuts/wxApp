cc5be444670e71bd5b74b3c1d1bb1dd7108587cb
伟业 刘 <Rain580@163.com>
2019-08-13 10:44:20 +0800

    Dialog

--- wxApp/ViewModels/MainWindowViewModel.cs
+++ wxApp/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Commands;
+﻿using MahApps.Metro.Controls.Dialogs;
+using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
 using Prism.Regions;
@@ -16,9 +17,11 @@ namespace wxApp.ViewModels
     {
         IEventAggregator _ea;
         IRegionManager _regionManager;
+        // Variable
+        private IDialogCoordinator dialogCoordinator;
         public MainWindowViewModel(IEventAggregator ea, IRegionManager regionManager)
         {
-
+            dialogCoordinator = DialogCoordinator.Instance;
             _ea = ea;
             _regionManager = regionManager;
             IsLoginFailed = true;
@@ -33,11 +36,49 @@ namespace wxApp.ViewModels
             CloseDelegateCommand = new DelegateCommand(CloseExecute, CanExecute);
             MinimizedDelegateCommand = new DelegateCommand(MinimizedExecute, CanExecute);
             MaximizedDelegateCommand = new DelegateCommand(MaximizedExecute, CanExecute);
-            
+            TopmostDelegateCommand = new DelegateCommand(TopmostExecute, CanExecute);
 
             _ea.GetEvent<MessageSentEvent>().Subscribe(MessageReceived);
+            _ea.GetEvent<SearcchSentEvent>().Subscribe(SearcchReceived);
         }
         #region method
+        public async Task<MessageDialogResult>  ShowMessageButtonDialog(string header, string message)
+        {
+            var mySettings = new MetroDialogSettings()
+            {
+                AffirmativeButtonText = "确定",
+                NegativeButtonText = "取消",
+                ColorScheme = MetroDialogColorScheme.Accented
+            };
+
+            var result =  await dialogCoordinator.ShowMessageAsync(this,header, message, MessageDialogStyle.AffirmativeAndNegative, mySettings);
+            return result;
+  
+        }
+        /// <summary>
+        /// 显示弹框
+        /// </summary>
+        public async void ShowMessageDialog(string header,string message)
+        {
+            await dialogCoordinator.ShowMessageAsync(this, header, message);
+        }
+
+        private async void RunProgressFromVm(string header, string message)
+        {
+            var controller = await dialogCoordinator.ShowProgressAsync(this, header, message);
+            controller.SetIndeterminate();
+
+            await TaskEx.Delay(3000);
+
+            await controller.CloseAsync();
+        }
+        public void SearcchReceived(string search)
+        {
+            ContentRegionOfCenterProperty = "ContentRegionOfContact";
+            CenterVisibilityProperty = Visibility.Hidden;
+            ContactVisibilityProperty = Visibility.Visible;
+            DynamicVisibilityProperty = Visibility.Hidden;
+        }
         private void MessageReceived(string message)
         {
             switch (message)
@@ -64,13 +105,33 @@ namespace wxApp.ViewModels
 
             //RegionManager.SetRegionManager("ContentRegionOfCenterControl", ContentRegionOfCenterProperty);
         }
-        //关闭窗口
-        public void CloseExecute()
+        public static class TaskEx
         {
+            public static Task Delay(int dueTime)
+            {
+                return Task.Delay(dueTime);
+            }
+        }
+        /// <summary>
+        /// 窗口置顶
+        /// </summary>
+        public void TopmostExecute()
+        {
+            string message = TopmostProperty ?"是否取消置顶":"是否置顶";
+            RunProgressFromVm("置顶提示", message);
+           // ShowMessageDialog("置顶提示", message);
+            TopmostProperty = TopmostProperty ? false:true;
+        }
+        //关闭窗口
+        public async void  CloseExecute()
+        {
+           var result = await ShowMessageButtonDialog("提示", "是否关闭当前窗口");
+            if (result == MessageDialogResult.Affirmative)
+                IsLoginFailed = false;
             //一般关闭当前窗体使用:
             //this.Close();
             //关闭全部窗体并退出程序使用：
-            Application.Current.Shutdown();
+            //Application.Current.Shutdown();
             // IsLoginFailed = false;
         }
         /// <summary>
@@ -122,6 +183,11 @@ namespace wxApp.ViewModels
         /// 最大化当前窗口
         /// </summary>
         public DelegateCommand MaximizedDelegateCommand { get; private set; }
+
+       /// <summary>
+       /// 设置窗口在最上方
+       /// </summary>
+        public DelegateCommand TopmostDelegateCommand { get; private set; }
         #endregion
         #region file
         private string _title;
@@ -213,6 +279,15 @@ namespace wxApp.ViewModels
             get { return _dynamicVisibilityProperty; }
             set { SetProperty(ref _dynamicVisibilityProperty, value); }
         }
+
+        private bool _topmostProperty;
+        //设置窗口在最上方
+        public bool TopmostProperty
+        {
+            get { return _topmostProperty; }
+            set { SetProperty(ref _topmostProperty, value); }
+        }
+        
         #endregion
     }
 }
