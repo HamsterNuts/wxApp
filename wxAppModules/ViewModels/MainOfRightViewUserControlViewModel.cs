5e301f408112322eb5ca30664c8207edad65bef2
伟业 刘 <Rain580@163.com>
2019-08-20 22:45:11 +0800

    update

--- wxAppModules/ViewModels/MainOfRightViewUserControlViewModel.cs
+++ wxAppModules/ViewModels/MainOfRightViewUserControlViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Events;
+﻿using Prism.Commands;
+using Prism.Events;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -10,25 +11,48 @@ using wxAppHelper.ViewModel;
 
 namespace wxAppModules.ViewModels
 {
-   public class MainOfRightViewUserControlViewModel:BindableBase
+    public class MainOfRightViewUserControlViewModel : BindableBase
     {
         IEventAggregator _ea;
+        int _contactId;
         public MainOfRightViewUserControlViewModel(IEventAggregator ea)
         {
             _ea = ea;
             _ea.GetEvent<TheContactChatRecordSentEvent>().Subscribe(TheContactChatRecordReceived);
             TheContactChatRecordReceived(1);
             HandChatRecordData.Add(new HandChatRecordViewModel() { IdProperty = 10, SourceIdProperty = 1, TargetIdProperty = 2, ContentProperty = "132" });
+            PublishMessageDelegateCommand = new DelegateCommand(PublishMessageMethod, () => true);
         }
-        #region method
+        #region Method
+        public void PublishMessageMethod()
+        {
+            wxAppHelper.Helper.InitializeData.HandChatRecordData.Add(new HandChatRecordViewModel()
+            {
+                IdProperty = wxAppHelper.Helper.InitializeData.HandChatRecordData.OrderByDescending(x => x.IdProperty).FirstOrDefault().IdProperty + 1,
+                SourceIdProperty = wxAppHelper.Helper.InitializeData.TheCurrentUserId,
+                TargetIdProperty = _contactId,
+                ContentProperty = MessageProperty,
+                RecordDateTime = DateTime.Now
+
+            });
+            MessageProperty = null;
+            HandChatRecordData = wxAppHelper.Helper.InitializeData.HandChatRecordData.Where(x => (x.SourceIdProperty == _contactId && x.TargetIdProperty == wxAppHelper.Helper.InitializeData.TheCurrentUserId) || (x.TargetIdProperty == _contactId && x.SourceIdProperty == wxAppHelper.Helper.InitializeData.TheCurrentUserId)).ToList();
+        }
         public void TheContactChatRecordReceived(int? contactId)
         {
-           var contact = wxAppHelper.Helper.InitializeData.HandContactData.Where(x => x.IdProperty == contactId).FirstOrDefault();
-           NameProperty =contact!=null? contact.NameProperty:null;
+            _contactId = Convert.ToInt32(contactId);
+            var contact = wxAppHelper.Helper.InitializeData.HandContactData.Where(x => x.IdProperty == contactId).FirstOrDefault();
+            NameProperty = contact != null ? contact.NameProperty : null;
             //获取当前用户和联系人的聊天记录
-           HandChatRecordData = wxAppHelper.Helper.InitializeData.HandChatRecordData.Where(x => (x.SourceIdProperty == contactId && x.TargetIdProperty == wxAppHelper.Helper.InitializeData.TheCurrentUserId) || (x.TargetIdProperty == contactId && x.SourceIdProperty == wxAppHelper.Helper.InitializeData.TheCurrentUserId)).ToList();
+            HandChatRecordData = wxAppHelper.Helper.InitializeData.HandChatRecordData.Where(x => (x.SourceIdProperty == contactId && x.TargetIdProperty == wxAppHelper.Helper.InitializeData.TheCurrentUserId) || (x.TargetIdProperty == contactId && x.SourceIdProperty == wxAppHelper.Helper.InitializeData.TheCurrentUserId)).ToList();
 
         }
+        #endregion
+        #region Command
+        /// <summary>
+        /// 设置窗口在最上方
+        /// </summary>
+        public DelegateCommand PublishMessageDelegateCommand { get; private set; }
         #endregion
         #region Property
         /// <summary>
@@ -49,6 +73,16 @@ namespace wxAppModules.ViewModels
             set { SetProperty(ref _handChatRecordData, value); }
         }
 
+        private string _messageProperty;
+
+        public string MessageProperty
+        {
+            get { return _messageProperty; }
+            set
+            {
+                SetProperty(ref _messageProperty, value);
+            }
+        }
         #endregion
     }
 }
